349ecfc2a8fe0d96f067fe279d594cabf9f47775
TestSathvik2007 <nikhilkrishnasathvikedu@gmail.com>
2026-03-06 21:04:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,10 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+
+    // 👇 ADD THIS LINE
+    chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
         manualChunks: undefined,
